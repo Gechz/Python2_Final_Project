dfffc7523f73880f3f4291c51cb4831899f45b7b
Gabriel Chapman <gechz96@gmail.com>
2024-11-16 16:15:21 +0100

    Update 6_🧪Simulation.py

--- project/pages/6_🧪Simulation.py
+++ project/pages/6_🧪Simulation.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sys
-import io
+#import sys
+#import io
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+#sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 
 
